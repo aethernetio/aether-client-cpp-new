aa3bd0417bd37e4a8083c0b02e22bc3f52d0ad4c
BartolomeyKant <notevill@gmail.com>
2025-01-18 17:20:50 +0500

    add test_SendingChunkList

--- tests/test-stream/safe-stream/test_safe_stream_types.cpp
+++ tests/test-stream/safe-stream/test_safe_stream_types.cpp
@@ -17,6 +17,7 @@
 #include "unity.h"
 
 #include "aether/stream_api/safe_stream/safe_stream_types.h"
+#include "aether/stream_api/safe_stream/sending_chunk_list.h"
 
 namespace ae::test_safe_stream_types {
 void test_OffsetRange() {
@@ -45,10 +46,43 @@ void test_OffsetRange() {
   TEST_ASSERT_TRUE(window_range.Before(SafeStreamRingIndex{110}));
   TEST_ASSERT_TRUE(window_range.After(SafeStreamRingIndex{110}));
 }
+
+void test_SendingChunkList() {
+  constexpr SafeStreamRingIndex::type window_size = 100;
+
+  SendingChunkList chunk_list{window_size};
+
+  // add three chunks
+  chunk_list.Register(SafeStreamRingIndex{0}, SafeStreamRingIndex{5}, Now());
+  chunk_list.Register(SafeStreamRingIndex{6}, SafeStreamRingIndex{10}, Now());
+  chunk_list.Register(SafeStreamRingIndex{11}, SafeStreamRingIndex{20}, Now());
+
+  TEST_ASSERT_EQUAL(3, chunk_list.size());
+  // merge two chunks
+  chunk_list.Register(SafeStreamRingIndex{0}, SafeStreamRingIndex{10}, Now());
+  TEST_ASSERT_EQUAL(2, chunk_list.size());
+  // merge again
+  chunk_list.Register(SafeStreamRingIndex{0}, SafeStreamRingIndex{20}, Now());
+  TEST_ASSERT_EQUAL(1, chunk_list.size());
+  // register a smaller chunk
+  chunk_list.Register(SafeStreamRingIndex{0}, SafeStreamRingIndex{10}, Now());
+  TEST_ASSERT_EQUAL(2, chunk_list.size());
+  // register a smaller between two chunks
+  chunk_list.Register(SafeStreamRingIndex{8}, SafeStreamRingIndex{14}, Now());
+  TEST_ASSERT_EQUAL(3, chunk_list.size());
+  auto& chunk = chunk_list.front();
+  TEST_ASSERT(SafeStreamRingIndex{15} == chunk.begin_offset);
+  TEST_ASSERT(SafeStreamRingIndex{20} == chunk.end_offset);
+  chunk_list.RemoveUpTo(SafeStreamRingIndex{7});
+  TEST_ASSERT_EQUAL(2, chunk_list.size());
+  chunk_list.RemoveUpTo(SafeStreamRingIndex{20});
+  TEST_ASSERT(chunk_list.empty());
+}
 }  // namespace ae::test_safe_stream_types
 
 int test_safe_stream_types() {
   UNITY_BEGIN();
   RUN_TEST(ae::test_safe_stream_types::test_OffsetRange);
+  RUN_TEST(ae::test_safe_stream_types::test_SendingChunkList);
   return UNITY_END();
 }
